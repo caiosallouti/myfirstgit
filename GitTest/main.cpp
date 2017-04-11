6a8886309c4d3db17bac0879415dae989e958ae0
Mauricio Matsubara <mauricio@matsubara.co>
2017-04-11 17:32:58 -0300

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by Mauricio Matsubara on 11/04/17.
+//  Copyright © 2017 Mauricio Matsubara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
